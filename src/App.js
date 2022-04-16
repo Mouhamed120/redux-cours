6d6e7c42508a00547ced59b7bb520325f5d72569
Mouhamed120 <65665285+Mouhamed120@users.noreply.github.com>
2022-04-16 06:22:02 +0200

    16/04/2022 Nous avons établi une liste dynamique

--- src/App.js
+++ src/App.js
@@ -1,14 +1,90 @@
-import logo from "./logo.svg";
-import Test from "./Test";
-import "./App.css";
+import { createSlice } from "@reduxjs/toolkit";
+import { useState } from "react";
 
-function App() {
+const cartSilice = createSlice({
+  name: "slice1"
+});
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  /**
+   * l'etat permet de décrire l'etate de l'application à un moment préci
+   *
+   * la vue : effectue un rendu en fonction de l'état
+   * action: ce sont de évènements qui se produise dans application
+   *
+   *
+   */
+  const [listeDesEleves, setListeDesEleves] = useState([
+    { id: 1, prenom: "Mouhamed" },
+    { id: 2, prenom: "Doudou" },
+    { id: 3, prenom: "Khadim" },
+    { id: 4, prenom: "kana" },
+    { id: 5, prenom: "nafi" },
+    { id: 6, prenom: "laye" }
+  ]);
+
   return (
-    <div className="App">
-      <Test />
-      <p>Hello it mine </p>
-    </div>
+    <>
+      {/* <button>compter :[ {counter} ]</button> */}
+      <User
+        listeDesEleves={listeDesEleves}
+        setListeDesEleves={setListeDesEleves}
+      />
+    </>
   );
-}
+};
 
+const User = ({ listeDesEleves, setListeDesEleves }) => {
+  const handleClick = (id) => {
+    const newListe = listeDesEleves.filter((item) => item.id !== id);
+    setListeDesEleves(newListe);
+  };
+
+  return (
+    <>
+      <Form
+        listeDesEleves={listeDesEleves}
+        setListeDesEleves={setListeDesEleves}
+      />
+
+      {listeDesEleves.map((i) => {
+        return (
+          <ul key={i.id}>
+            <li>{i.id}</li>
+            <li>
+              {i.prenom}{" "}
+              <button onClick={() => handleClick(i.id)}>Clicme</button>
+            </li>
+          </ul>
+        );
+      })}
+    </>
+  );
+};
+
+const Form = ({ listeDesEleves, setListeDesEleves }) => {
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newObj = { id: Date.now().toLocaleString(), prenom: name };
+    // setListeDesEleves(newObj);
+    setListeDesEleves((prev) => {
+      console.log(prev);
+      return [...prev, newObj];
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="name"
+        />
+      </form>
+    </>
+  );
+};
 export default App;
